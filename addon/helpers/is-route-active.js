2e586e614ca8da9ba0a54d791d80393ec1f1b83c
Lukas Kohler <lukas.kohler@ontheblueplanet.com>
2018-08-30 00:28:22 +0200

    observe currentURL

--- addon/helpers/is-route-active.js
+++ addon/helpers/is-route-active.js
@@ -1,10 +1,13 @@
 import Helper from '@ember/component/helper';
 import { inject as service } from '@ember/service';
+import { observer } from '@ember/object'
 
 export default Helper.extend({
   router: service(),
+  onChangeUrl: observer('router.currentURL', function() {
+    this.recompute();
+  }),
   compute([to]) {
-    debugger;
     return this.router.isActive(to);
-  }
+  },
 });
